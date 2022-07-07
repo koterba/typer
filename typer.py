025f33e19efc340a2677264b71b2fff331b73aef
alannxq <85573908+alannxq@users.noreply.github.com>
2022-07-07 13:17:56 +0100

    Add files via upload

---
+++ typer.py
@@ -0,0 +1,167 @@
+from curses import wrapper
+import curses
+import random
+import time
+
+sentences = [
+    "The miniature pet elephant became the envy of the neighborhood.",
+    "He decided to live his life by the big beats manifesto.",
+    "The light in his life was actually a fire burning all around him.",
+    "There aren't enough towels in the world to stop the sewage flowing from his mouth.",
+    "It's a skateboarding penguin with a sunhat!",
+    "Purple is the best city in the forest.",
+    "Kevin embraced his ability to be at the wrong place at the wrong time.",
+    "This book is sure to liquefy your brain.",
+    "My Mum tries to be cool by saying that she likes all the same things that I do.",
+    "Sarah ran from the serial killer holding a jug of milk.",
+    "Everything was going so well until I was accosted by a purple giraffe.",
+    "In the end, he realized he could see sound and hear words.",
+    "They got there early, and they got really good seats.",
+    "Seek success, but always be prepared for random cats.",
+    "He was an introvert that extroverts seemed to love.",
+    "Blue sounded too cold at the time and yet it seemed to work for gin.",
+    "It's always a good idea to seek shelter from the evil gaze of the sun.",
+    "He picked up trash in his spare time to dump in his neighbor's yard.",
+    "They throw cabbage that turns your brain into emotional baggage.",
+    "The lyrics of the song sounded like fingernails on a chalkboard.",
+    "Bill ran from the giraffe toward the dolphin.",
+    "Today arrived with a crash of my car through the garage door.",
+    "I would have gotten the promotion, but my attendance wasn’t good enough.",
+    "The crowd yells and screams for more memes.",
+    "The irony of the situation wasn't lost on anyone in the room.",
+    "I ate a sock because people on the Internet told me to.",
+    "She saw the brake lights, but not in time.",
+    "I’m working on a sweet potato farm."
+]
+
+
+def position_cursor(stdscr, target_text, current_text):
+    stdscr.move(curses.LINES // 2, (curses.COLS // 2 - len(target_text) // 2) + len(current_text) + 1)
+
+
+def print_ending(stdscr, wpm):
+    stdscr.clear()
+    stdscr.addstr(
+        curses.LINES // 2 - 2,
+        curses.COLS // 2 - len("Your scored:") // 2,
+        "Your scored:",
+    )
+    stdscr.addstr(
+        curses.LINES // 2,
+        curses.COLS // 2 - len(f"{wpm} WPM") // 2,
+        f"{wpm} WPM",
+        curses.color_pair(1)
+    )
+    while True:
+        try:
+            stdscr.getkey()
+            break
+        except:
+            pass
+
+
+def print_correct_word(stdscr, current_text: list[str], target_text: str):
+    if "".join(current_text) == target_text:
+        return True
+
+    for index, char in enumerate(current_text):
+        color = curses.color_pair(2) ## add red if they are wrong
+        if len(current_text) > len(target_text):
+            pass
+        elif char == target_text[index]:
+            color = curses.color_pair(1) ## add green if char both sentences match
+        stdscr.addstr(
+            curses.LINES // 2,
+            curses.COLS // 2 - len(target_text) // 2 + index + 1,
+            char,
+            color
+        )
+
+    position_cursor(stdscr, target_text, current_text)
+    #stdscr.move(curses.LINES // 2, ((curses.COLS // 2 - len(target_text)) + len(current_text)) + 5)
+    stdscr.addstr(target_text[len(current_text):], curses.color_pair(3)) ## fill in what is not written with grey
+
+    return False
+
+ 
+
+def wpm_test(stdscr, target_text):
+    current_text = []
+    start_time = time.time()
+    stdscr.nodelay(True)
+    stdscr.clear()
+
+    welcome_message = "Below is the sentence you will have to type:"
+    start_message = "Begin typing the sentence above to start."
+
+    stdscr.addstr(
+        (curses.LINES // 2) - 2,
+        (curses.COLS // 2) - len(welcome_message) // 2,
+        welcome_message
+    )
+    stdscr.addstr(
+        (curses.LINES // 2),
+        (curses.COLS // 2) - len(target_text) // 2,
+        target_text,
+        curses.color_pair(1)
+    )
+    stdscr.addstr(
+        (curses.LINES // 2) + 2,
+        (curses.COLS // 2) - len(start_message) // 2,
+        start_message
+    )
+    while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+        
+        if len(current_text) > 0:
+            wpm_text = f"WPM: {wpm}"
+
+            stdscr.addstr(
+                (curses.LINES // 2) - 2,
+                (curses.COLS // 2) - len(wpm_text) // 2,
+                wpm_text
+            )
+
+            position_cursor(stdscr, target_text, current_text)
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
+        
+        try:
+            ord(key)
+        except:
+            key = 0
+
+        if ord(key) == 27:
+            break
+        elif ord(key) == 8 and len(current_text) > 0:
+            current_text.pop()
+        else:
+            current_text.append(key)
+        
+        stdscr.clear()
+
+
+        if print_correct_word(stdscr, current_text, target_text):
+            print_ending(stdscr, wpm)
+            break
+        
+        position_cursor(stdscr, target_text, current_text)
+    
+        stdscr.refresh()
+
+    
+
+def main(stdscr):
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, 8, -1) ## grey
+    
+    ## start_screen(stdscr)
+    wpm_test(stdscr, target_text=random.choice(sentences))
+
+wrapper(main)
