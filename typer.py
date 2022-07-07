afd8fb98603b6fdaf748d89a46aa2edfa1639a36
alannxq <85573908+alannxq@users.noreply.github.com>
2022-07-07 15:16:31 +0100

    Update typer.py

--- typer.py
+++ typer.py
@@ -31,15 +31,21 @@ sentences = [
     "The irony of the situation wasn't lost on anyone in the room.",
     "I ate a sock because people on the Internet told me to.",
     "She saw the brake lights, but not in time.",
-    "I’m working on a sweet potato farm."
+    "I’m working on a sweet potato farm.",
+    "the simplest sentence here"
 ]
 
 
 def position_cursor(stdscr, target_text, current_text):
+    ''' Cursor is meaningless, but for ease of use and aesthetics
+        the cursor has to be manually moved to the letter/word that
+        the user is currently typing'''
     stdscr.move(curses.LINES // 2, (curses.COLS // 2 - len(target_text) // 2) + len(current_text) + 1)
 
 
 def print_ending(stdscr, wpm):
+    '''clears the screen, outputs results
+        waits for enter, else it quits the game'''
     stdscr.clear()
     stdscr.addstr(
         curses.LINES // 2 - 2,
@@ -52,28 +58,41 @@ def print_ending(stdscr, wpm):
         f"{wpm} WPM",
         curses.color_pair(1)
     )
+    stdscr.addstr(
+        curses.LINES // 2 + 2,
+        curses.COLS // 2 - len("Press enter to play again") // 2,
+        "Press enter to play again"
+    )
     while True:
         try:
-            stdscr.getkey()
-            break
+            key = stdscr.getkey()
+            if ord(key) == 10: ## 10 is enter
+                stdscr.clear()
+                wrapper(main) ## run game again
+            else:
+                break ## if enter is not pressed, quit the loop / quit the game
         except:
-            pass
+            pass ## as getkey is not blocking, it will throw an exception every time a user does not press a key
 
 
 def print_correct_word(stdscr, current_text: list[str], target_text: str):
+    ''' checks for correct location to print the green, red and grey characters
+        returns TRUE if the user has successfully finished and the sentences match'''
     if "".join(current_text) == target_text:
         return True
 
     for index, char in enumerate(current_text):
         color = curses.color_pair(2) ## add red if they are wrong
+        char_to_show = target_text[index]
         if len(current_text) > len(target_text):
             pass
         elif char == target_text[index]:
             color = curses.color_pair(1) ## add green if char both sentences match
+            char_to_show = char
         stdscr.addstr(
             curses.LINES // 2,
             curses.COLS // 2 - len(target_text) // 2 + index + 1,
-            char,
+            char_to_show,
             color
         )
 
@@ -86,6 +105,7 @@ def print_correct_word(stdscr, current_text: list[str], target_text: str):
  
 
 def wpm_test(stdscr, target_text):
+    '''Main loop of the game, calls all the functions above'''
     current_text = []
     start_time = time.time()
     stdscr.nodelay(True)
@@ -130,7 +150,7 @@ def wpm_test(stdscr, target_text):
             continue
         
         try:
-            ord(key)
+            ord(key) ## if the key is KEY_DOWN, for example, it will be changed to 0
         except:
             key = 0
 
@@ -138,30 +158,27 @@ def wpm_test(stdscr, target_text):
             break
         elif ord(key) == 8 and len(current_text) > 0:
             current_text.pop()
-        else:
+        elif len(current_text) +1 <= len(target_text):
             current_text.append(key)
-        
+   
         stdscr.clear()
-
-
+    
         if print_correct_word(stdscr, current_text, target_text):
             print_ending(stdscr, wpm)
             break
-        
+
         position_cursor(stdscr, target_text, current_text)
     
         stdscr.refresh()
 
     
-
 def main(stdscr):
     curses.start_color()
     curses.use_default_colors()
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_RED)
     curses.init_pair(3, 8, -1) ## grey
-    
-    ## start_screen(stdscr)
-    wpm_test(stdscr, target_text=random.choice(sentences))
+
+    wpm_test(stdscr, target_text=random.choice(sentences)) ## starts the game
 
 wrapper(main)
